1d42eee155459f61576b4580e0dd78d6e53c7f54
GsRms <1403.gustavo.ramos@gmail.com>
2024-11-23 16:36:11 -0300

    Correção Login
    Correção Login

--- A3/app.py
+++ A3/app.py
@@ -4,7 +4,7 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key = 'hjsewffdbasbdasewrdewsssdkskldsajkodnasjkdjk'
+app.secret_key = 'hjsewffdbasbdasewrdewsssdkskldfsfjkodnasjkdjk'
 db_config = {
     'host': 'localhost',
     'user': 'root',
